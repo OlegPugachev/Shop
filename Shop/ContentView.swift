d0a5e33266178b7f8218d3629dac2248e827dc70
Oleg <pugachev_o@icloud.com>
2024-12-12 11:21:29 +0200

    Initial commit

---
+++ Shop/ContentView.swift
@@ -0,0 +1,74 @@
+
+import SwiftUI
+
+struct ContentView: View {
+    @State private var email = ""
+    @State private var password = ""
+    
+    var body: some View {
+        VStack(spacing: 20) {
+            Text("Authorisation")
+                .padding()
+                .padding(.horizontal, 60)
+                .font(.title2.bold())
+                .background(.whiteAlpha)
+                .cornerRadius(12)
+            
+            VStack {
+                TextField("Email", text: $email)
+                    .padding()
+                    .background(.whiteAlpha)
+                    .cornerRadius(12)
+                    .padding(8)
+                    .padding(.horizontal, 12)
+                
+                SecureField("Password", text: $password)
+                    .padding()
+                    .background(.whiteAlpha)
+                    .cornerRadius(12)
+                    .padding(8)
+                    .padding(.horizontal, 12)
+                
+                Button {
+                    print("Login")
+                } label: {
+                    Text("Login")
+                        .padding()
+                        .frame(maxWidth: .infinity)
+                        .background(LinearGradient(colors: [.yellow, .orange], startPoint: .leading, endPoint: .trailing))
+                        .cornerRadius(12)
+                        .padding(8)
+                        .padding(.horizontal, 12)
+                        .font(.title3.bold())
+                        .foregroundColor(.black)
+                }
+            
+                Button {
+                    print("Still Not With Us?")
+                } label: {
+                    Text("Still not with us?")
+                        .padding(.horizontal)
+                        .frame(maxWidth: .infinity)
+                        .cornerRadius(12)
+                        .padding(8)
+                        .padding(.horizontal, 12)
+                        .font(.title3.bold())
+                        .foregroundColor(.secondary)
+                }
+                
+            }
+            .padding()
+            .padding(.top, 20)
+            .background(.whiteAlpha)
+            .cornerRadius(20)
+            .padding(20)
+            
+        }
+        .frame(maxWidth: .infinity, maxHeight: .infinity)
+        .background( Image("background").resizable().ignoresSafeArea())
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Shop/ShopApp.swift
@@ -0,0 +1,17 @@
+//
+//  ShopApp.swift
+//  Shop
+//
+//  Created by Oleg on 12.12.2024.
+//
+
+import SwiftUI
+
+@main
+struct ShopApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
